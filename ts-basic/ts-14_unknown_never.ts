51f61770610f59ffe7a03c371ecd09a5c80de5f8
hiruruku <30259989+hiruruku@users.noreply.github.com>
2020-05-25 22:44:59 +0900

    ts-14_unknown_never.ts

--- ts-basic/ts-14_unknown_never.ts
+++ ts-basic/ts-14_unknown_never.ts
@@ -15,3 +15,10 @@ userInput = 'Max';
 if(typeof userInput === 'string') {
   userName = userInput;
 }
+/**
+ * 
+ */
+function generateError(message: string, code: number): never{
+  throw { message: message,errorCode: code };
+}
+generateError('An error occurred!',500);
